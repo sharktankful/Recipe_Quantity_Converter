bc2a3ad700226dbb31fcb9a3a9bf9434aafeae88
Alec Smith <alecmsmith18@gmail.com>
2022-08-30 09:40:05 -0500

    Fixed "While Loop" Operator_lower error.

--- Recipe_Converter.py
+++ Recipe_Converter.py
@@ -1,5 +1,15 @@
-recipe_name = (input("Please enter in the name of your recipe here: "))
+recipe_name = input("Please enter in the name of your recipe here: ")
 
+math_operator = input(
+    f"Do you want {recipe_name} multiplied or divided? Enter here: ")
 
-math_operator = (
-    input(f"Do you want {recipe_name} multiplied or divided? Enter here: "))
+operator_lower = math_operator.lower()
+
+while operator_lower != "multiplied" and operator_lower != "divided":
+    print("\x1B[4m" + "This is not the correct choice." + "\x1B[0m")
+    math_operator = input(
+        f"Do you want {recipe_name} multiplied or divided? Enter here: ")
+    operator_lower = math_operator.lower()
+
+operator_amount = int(
+    input(f"By how much do you want it {operator_lower}? Enter here: "))
