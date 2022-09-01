24369dc602438c56b10332969ee6efbd0452581b
Alec Smith <alecmsmith18@gmail.com>
2022-09-01 13:36:46 -0500

    Gave error message an underline/reworded statement

--- defined_functions.py
+++ defined_functions.py
@@ -1,5 +1,5 @@
-#Checks if integer given is actually a integer
-def prompt_for_int(prompt_str=None, error_str="This is not an integer"):
+# Checks if integer given is actually a integer
+def prompt_for_int(prompt_str=None, error_str="\x1B[4m" + "Please enter in a number" + "\x1B[0m"):
     while True:
         try:
             return int(input(prompt_str))
