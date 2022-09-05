fd481140465d01597dd4aa15554b30bae0fda208
Alec Smith <alecmsmith18@gmail.com>
2022-09-04 22:43:55 -0500

    Created a seperate function for checking for ints.

--- defined_functions.py
+++ defined_functions.py
@@ -2,9 +2,18 @@ from fractions import Fraction
 
 
 # Checks if number/fraction given is actually a number/fraction
-def prompt_for_int(prompt_str=None, error_str="\x1B[4m" + "Please enter in a number" + "\x1B[0m"):
+def prompt_for_frac(prompt_str=None, error_str="\x1B[4m" + "Please enter in a number" + "\x1B[0m"):
     while True:
         try:
             return float(Fraction(input(prompt_str)))
+        except ValueError:
+            print(error_str, flush=True)
+
+
+# Checks if integer given is actually a integer
+def prompt_for_int(prompt_str=None, error_str="\x1B[4m" + "Please enter in a number" + "\x1B[0m"):
+    while True:
+        try:
+            return int(input(prompt_str))
         except ValueError:
             print(error_str, flush=True)
