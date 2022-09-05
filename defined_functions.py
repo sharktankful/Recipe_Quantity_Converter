a4d57656729c40050626318bae0fe9d17297ca20
Alec Smith <alecmsmith18@gmail.com>
2022-09-04 22:28:56 -0500

    Changed amount input to allow fractions as input.

--- defined_functions.py
+++ defined_functions.py
@@ -1,7 +1,10 @@
-# Checks if integer given is actually a integer
+from fractions import Fraction
+
+
+# Checks if number/fraction given is actually a number/fraction
 def prompt_for_int(prompt_str=None, error_str="\x1B[4m" + "Please enter in a number" + "\x1B[0m"):
     while True:
         try:
-            return int(input(prompt_str))
+            return float(Fraction(input(prompt_str)))
         except ValueError:
             print(error_str, flush=True)
